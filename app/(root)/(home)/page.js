fff6ada2c8374f646daca1c3a7978b0997a50a05
Ivan Barrios <ivanbar8112003@gmail.com>
2024-01-22 08:38:59 -0300

    General project structure

---
+++ app/(root)/(home)/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div>
+      {/* <Hero /> */}
+      {/* <AboutUs/> */}
+      {/* <Services/> */}
+      {/* <Testimonials/> */}
+      {/* <Contact/> */}
+      </div>
+    </main>
+  );
+}
